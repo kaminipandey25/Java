4c9fa8db5790fee87c379606ae86e5920bb22ffc
Kamini Pandey <91623323+kaminipandey25@users.noreply.github.com>
2023-06-18 22:10:41 +0530

    Create Recursion.java

---
+++ Recursion.java
@@ -0,0 +1,17 @@
+print number 5 to 1
+
+ import java.util.*;
+ public class recursion {
+     public static void printNumb(int n){
+         if(n==0){
+             return;
+         }
+         System.out.println(n);
+         printNumb(n-1);
+     }
+     public static void main(String args[]){
+         int n=5;
+         printNumb(n);
+     }
+    
+}

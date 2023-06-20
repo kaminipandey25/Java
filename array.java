310e0df3bb5193b539ac778c8ce1050232a7cc77
Kamini Pandey <91623323+kaminipandey25@users.noreply.github.com>
2023-06-20 22:39:39 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,12 @@
+//Write a Java program to print the elements of an array in reverse order.
+
+import java.util.*;
+public class Practicepaper{
+    public static void main(String args[]){
+        int marks[]= {5,6,4,7,9,4};
+        for(int i=marks.length-1;i>=0;i--){
+            System.out.println(marks[i]);
+           
+        }
+    }
+}

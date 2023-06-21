565a08a926dfd4afb7a9d5a80084369c390dc048
Kamini Pandey <91623323+kaminipandey25@users.noreply.github.com>
2023-06-21 07:05:18 +0530

    Create sumofarray.java

---
+++ sumofarray.java
@@ -0,0 +1,15 @@
+//Create an array of 5 floats and calculate their sum.
+ public class Practicepaper{
+     public static void main(String[] args){
+         float[] marks={0.3f,0.4f,0.5f,0.7f,0.2f};
+         float sum=0;
+        
+         for(float i:marks){
+             sum=sum+i;
+            
+         }
+         System.out.print("the value of total is "+sum);
+
+     }
+
+ }

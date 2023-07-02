95a0a5ea4578e74602c40a2e343a1a233033910c
Kamini Pandey <91623323+kaminipandey25@users.noreply.github.com>
2023-07-02 21:26:28 +0530

    Create piramindpatttern.java

---
+++ piramindpatttern.java
@@ -0,0 +1,21 @@
+// TO PRINT STAR PIRAMIND
+
+import java.util.*;
+ public class recursion{
+     public static void main(String args[]){
+         int n=5;
+
+         for(int numstar=1;numstar<=n;numstar++){
+             int numspace=n-numstar;
+             for(int i=0;i<numstar;i++){
+                 System.out.print("*");
+             }
+             for(int i=0;i<numspace;i++){
+                 System.out.print(" ");
+             }
+             System.out.println();
+         }
+
+
+    }
+ }

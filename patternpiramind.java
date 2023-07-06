80156602ce5a94cc792d0e20703e1e7b09bfcca6
Kamini Pandey <91623323+kaminipandey25@users.noreply.github.com>
2023-07-06 23:23:27 +0530

    Create patternpiramind.java

---
+++ patternpiramind.java
@@ -0,0 +1,18 @@
+//TO PRINT STAR PIRAMIND OPPOSITE
+
+ import java.util.*;
+ public class recursion{
+     public  static void main(String args[]){
+         int n=5;
+         for(int numstar=1;numstar<=n;numstar++){
+             int numspace=n-numstar;
+             for(int i=0;i<numspace;i++){
+                 System.out.print(" ");
+             }
+             for(int i=0;i<numstar;i++){
+                 System.out.print("*");
+             }
+             System.out.println();
+         }
+     }
+ }

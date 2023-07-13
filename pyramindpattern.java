082e6bc9e5ed771c9bdde9987ac4cb05657f9ffc
Kamini Pandey <91623323+kaminipandey25@users.noreply.github.com>
2023-07-13 21:51:03 +0530

    Create pyramindpattern.java

---
+++ pyramindpattern.java
@@ -0,0 +1,17 @@
+// PRINT INVERTED PIRAMIND
+
+ import java.util.*;
+  public class recursion{
+     public static void main(String args[]){
+         int n=5;
+         for(int i=0;i<=n;i++){
+             for(int i=0;i<=n-i;i++){
+             System.out.print(" ");
+             System.out.print("*");
+         }
+        
+        
+         System.out.println();
+     }
+ } 
+}

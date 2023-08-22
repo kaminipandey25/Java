2ed72ee538117f155f43fa8b0bd160551cb24031
Kamini Pandey <91623323+kaminipandey25@users.noreply.github.com>
2023-08-22 23:23:47 +0530

    Create reversearray.java

---
+++ reversearray.java
@@ -0,0 +1,9 @@
+//PRINT ARRAY IN REVERSE
+class Practicepaper{
+    public static void main(String args[]){
+        int arr[]={3,4,5,6,7,8};
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.println(i);
+        }
+    }
+} 
